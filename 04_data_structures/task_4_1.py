f8b1d741530a28fc0c14c49354a594b034458e1e
Strashila <vstrashila@mail.ru>
2022-01-14 12:12:45 +0300

    init files for PyNEng Project

---
+++ 04_data_structures/task_4_1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+'''
+Задание 4.1
+
+Обработать строку nat таким образом,
+чтобы в имени интерфейса вместо FastEthernet было GigabitEthernet.
+
+Ограничение: Все задания надо выполнять используя только пройденные темы.
+
+'''
+
+nat = 'ip nat inside source list ACL interface FastEthernet0/1 overload'
+nat.replace('Fast', 'Gigabit')
